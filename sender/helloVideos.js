6215ae2947015c2cebd674959a3456d30600b603
vessp <dwr169@gmail.com>
2014-10-07 06:29:52 -0700

    -

--- sender/helloVideos.js
+++ sender/helloVideos.js
@@ -46,8 +46,8 @@ function initializeCastApi() {
   
   //var applicationID = '0FD55629';
   //var applicationID = '705DB4F3';
-  //var applicationID = '0FD55629';//github receiver
-  var applicationID = '095E018A';//styled receiver
+  var applicationID = '0FD55629';//github receiver
+  //var applicationID = '095E018A';//styled receiver
   
   var sessionRequest = new chrome.cast.SessionRequest(applicationID, 'asdf');
   var apiConfig = new chrome.cast.ApiConfig(sessionRequest,
@@ -233,7 +233,7 @@ function loadMedia() {
   console.log("loading..." + currentMediaURL);
   appendMessage("loading..." + currentMediaURL);
   var mediaInfo = new chrome.cast.media.MediaInfo(currentMediaURL);
-  mediaInfo.contentType = 'video/mp4';
+  mediaInfo.contentType = 'application/x-mpegurl';//'video/mp4';
   var request = new chrome.cast.media.LoadRequest(mediaInfo);
   request.autoplay = true;
   request.currentTime = 0;
