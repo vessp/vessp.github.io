e096d1310e38aabbacb9fbf708f3a8530bfea9e1
vessp <dwr169@gmail.com>
2014-10-07 05:14:00 -0700

    -

--- sender-dev/helloVideos.js
+++ sender-dev/helloVideos.js
@@ -46,10 +46,10 @@ function initializeCastApi() {
   
   //var applicationID = '0FD55629';
   //var applicationID = '705DB4F3';
-  //var applicationID = '0FD55629';//github custom receiver
+  var applicationID = '0FD55629';//github custom receiver
   //var applicationID = '9B4052E5';//github custom dev receiver
   //var applicationID = '095E018A';//github styled receiver
-  var applicationID = 'D75AF013'; //github sample player
+  //var applicationID = 'D75AF013'; //github sample player
   
   var sessionRequest = new chrome.cast.SessionRequest(applicationID, 
 	[chrome.cast.Capability.AUDIO_OUT, chrome.cast.Capability.VIDEO_OUT], 30000);
