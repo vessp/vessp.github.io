12d75d4c2ba840f8d9072b24058ef8c3188fb25f
vessp <dwr169@gmail.com>
2014-10-07 06:54:59 -0700

    -

--- sender/helloVideos.js
+++ sender/helloVideos.js
@@ -233,7 +233,7 @@ function loadMedia() {
   console.log("loading..." + currentMediaURL);
   appendMessage("loading..." + currentMediaURL);
   var mediaInfo = new chrome.cast.media.MediaInfo(currentMediaURL);
-  mediaInfo.contentType = 'application/x-mpegurl';//'video/mp4';
+  mediaInfo.contentType = 'video/mp4';
   var request = new chrome.cast.media.LoadRequest(mediaInfo);
   request.autoplay = true;
   request.currentTime = 0;
