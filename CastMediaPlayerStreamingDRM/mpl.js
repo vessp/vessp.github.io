6681b9acee98b41e34c602cc2d378c5f9726b5a0
vessp <dwr169@gmail.com>
2014-10-07 05:09:12 -0700

    -

--- CastMediaPlayerStreamingDRM/mpl.js
+++ CastMediaPlayerStreamingDRM/mpl.js
@@ -624,8 +624,9 @@ onload = function() {
           // example of setting CORS withCredentials
           requestInfo.withCredentials = true;
           // example of setting headers
-          //requestInfo.headers = {};
+          requestInfo.headers = {};
           //requestInfo.headers['content-type'] = 'text/xml;charset=utf-8';
+		  requestInfo.headers['origin'] = 'https://vessp.github.io';
         };
       }
       if (licenseCredentials) {
